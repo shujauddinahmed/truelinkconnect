df41b534bcdacdd88b46b425bfeaa20b2c83a7f6
Shujauddin <68898579+shujauddinahmed@users.noreply.github.com>
2025-03-01 02:23:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,17 @@ $(document).ready(function () {
         };
 
         $.ajax({
-            url: "https://script.google.com/macros/s/AKfycbwv2IQ92vwBYQPWO8mGt6ubx-Co_4FcgBL5PsDU89r_sRRF2a_dhzE9KuepVBYROMVpRg/exec", // Replace with your actual script URL
+            url: "https://script.google.com/macros/s/AKfycbzag4GDhKtdGiwcQ-1LQY5oBYHqX65G0I8AJzy0TPnGtM3jhBKgAPh8sIuCAZJZi4fGoQ/exec", // Replace with actual Web App URL
             type: "POST",
             data: formData,
+            crossDomain: true,
+            dataType: "json",
             success: function (response) {
                 alert("Form submitted successfully!");
                 $("#contactForm")[0].reset();
             },
-            error: function () {
+            error: function (xhr, status, error) {
+                console.log(xhr.responseText);
                 alert("There was an error. Try again.");
             }
         });
