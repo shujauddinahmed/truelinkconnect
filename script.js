d24bf830fd4d0cc9d7fb0415cf854891f38bad3b
Shujauddin <68898579+shujauddinahmed@users.noreply.github.com>
2025-03-01 03:14:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,14 +15,13 @@ $(document).ready(function () {
             expectedUserBase: $("#expectedUserBase").val(),
             launchDate: $("#launchDate").val()
         };
-
         $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbzag4GDhKtdGiwcQ-1LQY5oBYHqX65G0I8AJzy0TPnGtM3jhBKgAPh8sIuCAZJZi4fGoQ/exec", // Replace with your actual Web App URL
+    url: "https://script.google.com/macros/s/AKfycbzag4GDhKtdGiwcQ-1LQY5oBYHqX65G0I8AJzy0TPnGtM3jhBKgAPh8sIuCAZJZi4fGoQ/exec", // Replace with your Web App URL
     type: "POST",
     data: formData,
     crossDomain: true,
     success: function (response) {
-        console.log("Raw Response:", response); // Debugging
+        console.log("Raw Response:", response);
         
         if (response.status === "success") {
             alert("Form submitted successfully!");
